8788a578b95d8f971fb6bfca6e4767d1b80d6ca9
keithkat <keith110606@gmail.com>
2026-07-03 22:31:40 +0800

    Adding firstNumber logic

--- script.js
+++ script.js
@@ -1,12 +1,57 @@
 const calculatorOperation = document.querySelectorAll('.operation-buttons');
 const calculatorNumber = document.querySelectorAll(".calculator-number")
+let firstNum = "";
+let secondNum;
+let ans = 0;
+let op;
 
-console.log(calculatorNumber)
-console.log(calculatorOperation)
+const handleButton = (numButton) => {
+    let button = numButton.target;
+    switch (button.id) {
+        case "0":
+            firstNum = firstNum + "0"
+            console.log(firstNum)
+            break;
+        case "1":
+            firstNum = firstNum + "1"
+            console.log(firstNum)
+            break;
+        case "2":
+            firstNum = firstNum + "2"
+            console.log(firstNum)
+            break;
+        case "3":
+            firstNum = firstNum + "3"
+            console.log(firstNum)           
+            break;
+        case "4":
+            firstNum = firstNum + "4"
+            console.log(firstNum)
+            break;
+        case "5":
+            firstNum = firstNum + "5"
+            console.log(firstNum)
+            break;
+        case "6":
+            firstNum = firstNum + "6"
+            console.log(firstNum)
+            break;
+        case "7":
+            firstNum = firstNum + "7"
+            console.log(firstNum)
+            break;
+        case "8":
+            firstNum = firstNum + "8"
+            console.log(firstNum);
+            break;
+        case "9":
+            firstNum = firstNum + "9"
+            console.log(firstNum)
+            break;
+    };
+};
 
-calculatorNumber.forEach(button => button.addEventListener('click', (handleButton) => {
-    let button = button.target;
-}));
+calculatorNumber.forEach(button => button.addEventListener('click', handleButton));
 
 const operate = (a, b, operator) => {
     switch (operator) {
