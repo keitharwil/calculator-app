9881f4a9ed484f41832e9520e03c7fa63ae7563f
keithkat <keith110606@gmail.com>
2026-07-05 01:00:45 +0800

    Add Logic switching firstNum secondNum

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const calculatorOperation = document.querySelectorAll('.operation-buttons');
-const calculatorNumber = document.querySelectorAll(".calculator-number")
+const calculatorNumber = document.querySelectorAll(".calculator-number");
 const calValues = {
     firstNum    :   "0", 
     secondNum   :   "0",
@@ -7,9 +7,7 @@ const calValues = {
     opIsClicked :   false,
     ans         :   0
 }
-
-const opHandler = (isOpClicked) => {
-    const operatorBtn = (opButton) => {
+const operatorBtn = (opButton) => {
     let button = opButton.target;
     switch (button.id) {
         case "+":
@@ -43,67 +41,74 @@ const opHandler = (isOpClicked) => {
             console.log(calValues.opIsClicked)
             break;
     };
-};
 
+};
 calculatorOperation.forEach(button => button.addEventListener('click', operatorBtn));
+
+const numHandler = (numButton) => {
+    let numToChange = "firstNum"
+    let button = numButton.target;
+    calValues.opIsClicked ? numToChange = "secondNum" : null
+    switch (button.id) {
+        case "0":
+            calValues[numToChange] === "0" ? calValues[numToChange] = "0" :
+                calValues[numToChange] = calValues[numToChange] + "0"
+                console.log(calValues[numToChange])
+                break;  
+        case "1":
+            calValues[numToChange] === "0" ? calValues[numToChange] = "1" :
+                calValues[numToChange] = calValues[numToChange] + "1"
+                console.log(calValues[numToChange])
+                break;
+        case "2":
+            calValues[numToChange] === "0" ? calValues[numToChange] = "2" :
+                calValues[numToChange] = calValues[numToChange] + "2"
+                console.log(calValues[numToChange])
+                break;
+        case "3":
+            calValues[numToChange] === "0" ? calValues[numToChange] = "3" :
+                calValues[numToChange] = calValues[numToChange] + "3"
+                console.log(calValues[numToChange])           
+                break;
+        case "4":
+            calValues[numToChange] === "0" ? calValues[numToChange] = "4" :
+                calValues[numToChange] = calValues[numToChange] + "4"
+                console.log(calValues[numToChange])
+                break;
+        case "5":
+            calValues[numToChange] === "0" ? calValues[numToChange] = "5" :
+                calValues[numToChange] = calValues[numToChange] + "5"
+                console.log(calValues[numToChange])
+                break;
+        case "6":
+            calValues[numToChange] === "0" ? calValues[numToChange] = "6" :
+                calValues[numToChange] = calValues[numToChange] + "6"
+                console.log(calValues[numToChange])
+                break;
+        case "7":
+            calValues[numToChange] === "0" ? calValues[numToChange] = "7" :
+                calValues[numToChange] = calValues[numToChange] + "7"
+                console.log(calValues[numToChange])
+                break;
+        case "8":
+            calValues[numToChange] === "0" ? calValues[numToChange] = "8" :
+                calValues[numToChange] = calValues[numToChange] + "8"
+                console.log(calValues[numToChange]);
+                break;
+        case "9":
+            calValues[numToChange] === "0" ? calValues[numToChange] = "9" :
+                calValues[numToChange] = calValues[numToChange] + "9"
+                console.log(calValues[numToChange])
+                break;
+        case "decimal-point":
+            calValues[numToChange].includes(".") ? null :
+                calValues[numToChange] = calValues[numToChange] + "."
+                console.log(calValues[numToChange])
+                break;
+    }; 
 };
 
-const numberHandler = (isOpClicked) => {
-        const firstNumHandler = (numButton) => {
-        let button = numButton.target;
-        // calValues.firstNum.length > 0 && calValues.firstNum[0] === "0" && calValues.firstNum.includes(".") ? calValues.firstNum = "" : null;
-        switch (button.id) {
-            case "0":
-                calValues.firstNum = calValues.firstNum + "0"
-                console.log(calValues.firstNum)
-                break;
-            case "1":
-                calValues.firstNum = calValues.firstNum + "1"
-                console.log(calValues.firstNum)
-                break;
-            case "2":
-                calValues.firstNum = calValues.firstNum + "2"
-                console.log(calValues.firstNum)
-                break;
-            case "3":
-                calValues.firstNum = calValues.firstNum + "3"
-                console.log(calValues.firstNum)           
-                break;
-            case "4":
-                calValues.firstNum = calValues.firstNum + "4"
-                console.log(calValues.firstNum)
-                break;
-            case "5":
-                calValues.firstNum = calValues.firstNum + "5"
-                console.log(calValues.firstNum)
-                break;
-            case "6":
-                calValues.firstNum = calValues.firstNum + "6"
-                console.log(calValues.firstNum)
-                break;
-            case "7":
-                calValues.firstNum = calValues.firstNum + "7"
-                console.log(calValues.firstNum)
-                break;
-            case "8":
-                calValues.firstNum = calValues.firstNum + "8"
-                console.log(calValues.firstNum);
-                break;
-            case "9":
-                calValues.firstNum = calValues.firstNum + "9"
-                console.log(calValues.firstNum)
-                break;
-            case "decimal-point":
-                if (!calValues.firstNum.includes(".")) {
-                calValues.firstNum = calValues.firstNum + "."
-                console.log(calValues.firstNum)
-                break;
-                }
-        }; 
-    };
-
-    calculatorNumber.forEach(button => button.addEventListener('click', firstNumHandler));
-};
+calculatorNumber.forEach(button => button.addEventListener('click', numHandler));  
 
 const operate = (a, b, operator) => {
     const add = (a, b) => {
@@ -139,6 +144,3 @@ const operate = (a, b, operator) => {
     };
     
 };
-
-numberHandler(calValues.opIsClicked);
-opHandler(calValues.opIsClicked);
